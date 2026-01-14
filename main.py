2eeb011fdde918dcc8e7d78f40fd18dd03f28a74
swwwjjw <zhukovskiy.ev2@gmail.com>
2026-01-14 19:02:27 +0300

    Edited port

--- main.py
+++ main.py
@@ -106,4 +106,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=1000, reload=True)
