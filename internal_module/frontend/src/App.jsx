ab4663e340c3f0e4bd93203abe5d0a5b5544c737
swwwjjw <zhukovskiy.ev2@gmail.com>
2026-02-03 16:53:28 +0300

    Edited chart

--- internal_module/frontend/src/App.jsx
+++ internal_module/frontend/src/App.jsx
@@ -146,9 +146,9 @@ function App() {
             )}
           </div>
           <div className="market-ticks">
-            <div className="val-p25">{Math.round(stats.metrics.min).toLocaleString()} ₽</div>
-            <div className="val-p50">{Math.round(stats.metrics.min + (stats.metric.max - stats.metrics.min) * 0.25).toLocaleString()} ₽</div>
-            <div className="val-p75">{Math.round(stats.metrics.min + (stats.metrics.max - stats.metrics.min) * 0.5).toLocaleString()} ₽</div>
+            <div className="val-p25">{Math.round(stats.metrics.min + (stats.metrics.max - stats.metrics.min) * 0.25).toLocaleString()} ₽</div>
+            <div className="val-p50">{Math.round(stats.metrics.median).toLocaleString()} ₽</div>
+            <div className="val-p75">{Math.round(stats.metrics.min + (stats.metrics.max - stats.metrics.min) * 0.75).toLocaleString()} ₽</div>
             <div className="val-max">{Math.round(stats.metrics.max).toLocaleString()} ₽</div>
           </div>
         </div>
